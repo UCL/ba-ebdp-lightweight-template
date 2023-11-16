fe949791381d2330391e0dad987a90a018e76bcc
Gareth Simons <garethsimons@me.com>
2023-11-16 15:39:09 +0000

    setting up

--- workflows/extent_download.py
+++ workflows/extent_download.py
@@ -1,11 +1,11 @@
 # %%
 import osmnx as ox
 
-# Specify the location and download boundaries
+# Download the boundaries for the necessary location
+# This will return a GeoPandas DataFrame
 cyprus_boundary = ox.geocode_to_gdf(
     ["Cyprus", "British Sovereign Base Areas", "Northern Cyprus"]
 )
-# the boundaries are now contained in a GeoPandas dataframe
 # dissolve the boundaries into a single boundary
 cyprus_boundary_merged = cyprus_boundary.dissolve()
 # then save to a file
