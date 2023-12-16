78be93040a3802f02b045f40e1922b85b2e881cb
Gareth Simons <garethsimons@me.com>
2023-12-16 21:03:46 +0000

    adds gothenburg tests

---
+++  cases/gothenburg.py
@@ -0,0 +1,77 @@
+"""
+Generates algorithmically cleaned primal network and runs centralities
+"""
+
+# %%
+import geopandas as gpd
+import osmnx as ox
+from cityseer import metrics
+from cityseer.tools import graphs, io
+from pyproj import Transformer
+from shapely import geometry
+
+# fetch using relation id
+gothenburg_extents_gdf = ox.geocode_to_gdf("R935611", by_osmid=True)
+# take convex hull
+gothenburg_extents_gdf.geometry = gothenburg_extents_gdf.geometry.convex_hull
+# save for QGIS
+gothenburg_extents_gdf.to_file(f"../temp/gothenburg_boundary.gpkg")
+
+# %%
+# read the extents file (in case edited from QGIS)
+extents_gpd = gpd.read_file(f"../temp/gothenburg_boundary.gpkg")
+# fetch geom
+extents_geom_wgs = extents_gpd.iloc[0].geometry
+# transform to 3007
+transformer = Transformer.from_crs("EPSG:4326", "EPSG:3007", always_xy=True)
+extents_geom = geometry.Polygon(
+    [transformer.transform(x, y) for x, y in extents_geom_wgs.exterior.coords]
+)
+# take convex hull, then buffer by 20km, and simplify by 100m
+extents_geom = extents_geom.convex_hull.buffer(20000).simplify(100)
+# a separate 10km buffered version for edge effects
+extents_geom_buff = extents_geom.buffer(10000)
+
+# %%
+# fetch and automatically clean
+G_clean = io.osm_graph_from_poly(
+    extents_geom_buff,
+    simplify=True,
+    poly_epsg_code=3007,
+    to_epsg_code=3007,
+)
+# save to QGIS
+edges_gdf_primal = io.geopandas_from_nx(G_clean, crs=3007)
+edges_gdf_primal.to_file(f"../temp/gothenburg_network_clean.gpkg")
+
+# %%
+# reopen in case edited in QGIS
+edges_gdf_qgis = gpd.read_file(f"../temp/gothenburg_network_clean.gpkg")
+G_qgis = io.nx_from_generic_geopandas(edges_gdf_qgis)
+# mark nodes inside original unbuffered extents as "live"
+for nd_key, nd_data in G_clean.nodes(data=True):
+    if extents_geom.contains(geometry.Point(nd_data["x"], nd_data["y"])):
+        G_clean.nodes[nd_key]["live"] = True
+    else:
+        G_clean.nodes[nd_key]["live"] = False
+# prepare data structures
+nodes_gdf, edges_gdf, network_structure = io.network_structure_from_nx(G_qgis, crs=3007)
+# generate vis lines for nodes - view in QGIS as "edge_geom"
+G_qgis = graphs.nx_generate_vis_lines(G_qgis)
+
+
+# copy to GPD
+def generate_vis_lines(node_row):
+    return G_qgis.nodes[node_row.name]["line_geom"]
+
+
+nodes_gdf["edge_geom"] = nodes_gdf.apply(generate_vis_lines, axis=1)
+
+# %%
+# compute centralities
+metrics.networks.node_centrality_shortest(
+    network_structure, nodes_gdf, distances=[500, 1000, 2000, 5000, 10000]
+)
+# save to GPKG
+nodes_gdf.to_file(f"../temp/gothenburg_network_clean_nodes.gpkg")
+nodes_gdf.to_file(f"../temp/gothenburg_network_clean_edges.gpkg")
---
+++  cases/gothernburg_official.py
@@ -0,0 +1,55 @@
+"""
+runs centralities on official network
+"""
+
+# %%
+import geopandas as gpd
+from cityseer import metrics
+from cityseer.tools import graphs, io
+from shapely import geometry, ops, to_wkt
+
+# reopen in case edited in QGIS
+edges_gdf = gpd.read_file(f"../temp/GOT_NMS_Revised_231115/GOT_NMS_Network_231115.shp")
+# Union all geometries in the GeoDataFrame
+extents_geom_buff = ops.unary_union(edges_gdf["geometry"]).convex_hull
+# a reverse buffer 10km for edge effects
+extents_geom = extents_geom_buff.buffer(-10000)
+# convert edges GDF to networkx
+G_official = io.nx_from_generic_geopandas(edges_gdf)
+# makes sure nodes are properly connected
+G_official = graphs.nx_consolidate_nodes(G_official, 2)
+# mark nodes inside original unbuffered extents as "live"
+for nd_key, nd_data in G_official.nodes(data=True):
+    if extents_geom.contains(geometry.Point(nd_data["x"], nd_data["y"])):
+        G_official.nodes[nd_key]["live"] = True
+    else:
+        G_official.nodes[nd_key]["live"] = False
+# prepare data structures
+nodes_gdf, edges_gdf, network_structure = io.network_structure_from_nx(
+    G_official, crs=3007
+)
+# generate vis lines for nodes - view in QGIS as "edge_geom"
+G_qgis = graphs.nx_generate_vis_lines(G_official)
+
+# %%
+# compute centralities
+metrics.networks.node_centrality_shortest(
+    network_structure, nodes_gdf, distances=[500, 1000, 2000, 5000, 10000]
+)
+
+
+def generate_vis_lines(node_row):
+    return G_qgis.nodes[node_row.name]["line_geom"]
+
+
+def geom_to_wkt(node_row):
+    return to_wkt(node_row["geom"])
+
+
+nodes_gdf["edge_geom"] = nodes_gdf.apply(generate_vis_lines, axis=1)
+nodes_gdf["node_geom_wkt"] = nodes_gdf.apply(geom_to_wkt, axis=1)
+nodes_gdf = nodes_gdf.drop(columns=["geom"])
+nodes_gdf = nodes_gdf.set_geometry("edge_geom")
+
+nodes_gdf.to_file(f"../temp/gothenburg_network_official_nodes.gpkg")
+edges_gdf.to_file(f"../temp/gothenburg_network_official_edges.gpkg")
